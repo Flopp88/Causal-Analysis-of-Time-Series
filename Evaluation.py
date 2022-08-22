ec64466600f0ab4940945fd35d08af74028dbc10
Flopp88 <93790527+Flopp88@users.noreply.github.com>
2022-08-22 10:16:18 +0200

    Add files via upload

--- Evaluation.py
+++ Evaluation.py
@@ -4,18 +4,25 @@ import networkx as nx
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+
+def remove_suffix(input_string, suffix):
+    if suffix and input_string.endswith(suffix):
+        return input_string[:-len(suffix)]
+    return input_string
 
 
 def evaluate(gtfile, predictedfile, nb_var, tau_max):
-    columns = [i for i in range(nb_var)]
-
     """Evaluates the results by comparing it to the ground truth graph, and calculating precision, recall and F1-score"""
 
-    readgt, delays = getdelays(gtfile, columns)
-    readpred, preddelays = getdelays(predictedfile, columns)
+    readgt, delays = getdelays(gtfile, nb_var)
+    readpred, preddelays = getdelays(predictedfile, nb_var)
 
-    gt_matrix = adjacency_matrix(delays, nb_var)
-    pred_matrix = adjacency_matrix(preddelays, nb_var)
+    preddelays = manage_unoriented_edges(readgt, delays, readpred, preddelays)
+
+    gt_matrix = weighted_adjacency_matrix(delays, nb_var)
+    pred_matrix = weighted_adjacency_matrix(preddelays, nb_var)
 
     frob = Frobenius(gt_matrix, pred_matrix)
     mse = MSE(gt_matrix, pred_matrix)
@@ -66,8 +73,8 @@ def evaluate(gtfile, predictedfile, nb_var, tau_max):
         F1direct = 0.
     print("F1 score: ", F1direct, "(includes only direct causal relationships)")
 
-    percentagecorrect = evaluatedelay(delays, preddelays, TPsdirect, tau_max) * 100
-    print("Percentage of delays that are correctly discovered: ", percentagecorrect, "%")
+    percentagecorrect = evaluatedelay(delays, preddelays, TPsdirect, tau_max)
+    print("Percentage of delays that are correctly discovered: ", percentagecorrect)
 
     print("Frobenius Norm: ", frob)
     print("Mean Squarred error: ", mse)
@@ -80,8 +87,9 @@ def evaluate(gtfile, predictedfile, nb_var, tau_max):
     return [tpr, fpr, precision, recall, F1direct, percentagecorrect, frob, mse]
 
 
-def getdelays(pddata, columns):
+def getdelays(pddata, nb_var):
     """Collects the total delay of indirect causal relationships."""
+    columns = [i for i in range(nb_var)]
 
     readgt = dict()
     effects = pddata[1]
@@ -96,7 +104,10 @@ def getdelays(pddata, columns):
         value = causes[i]
         if value not in readgt[key]:
             readgt[key].append(value)
-        pairdelays[(key, value)] = delays[i]
+        if (key, value) in pairdelays:
+            pairdelays[(key, value)] = min(delays[i], pairdelays[(key, value)])
+        else:
+            pairdelays[(key, value)] = delays[i]
         gtnrrelations += 1
 
     g = nx.DiGraph()
@@ -133,34 +144,41 @@ def evaluatedelay(gtdelays, alldelays, TPs, receptivefield):
         return zeros / float(total)
 
 
-def plotgraph(alldelays, columns):
+def plotgraph(alldelays, nb_var, edgecolor, selfcauses):
+    columns = [i for i in range(nb_var)]
     """Plots a temporal causal graph showing all discovered causal relationships annotated with the time delay between cause and effect."""
     G = nx.DiGraph()
     for c in columns:
         G.add_node(c)
     for pair in alldelays:
         p1, p2 = pair
-        nodepair = (columns[p2], columns[p1])
-
-        G.add_edges_from([nodepair], weight=alldelays[pair])
+        if selfcauses:
+            nodepair = (columns[p2], columns[p1])
+            G.add_edges_from([nodepair], weight=alldelays[pair], length=300)
+        else:
+            if p1 != p2:
+                nodepair = (columns[p2], columns[p1])
+                G.add_edges_from([nodepair], weight=alldelays[pair], length=300)
 
     edge_labels = dict([((u, v,), d['weight'])
                         for u, v, d in G.edges(data=True)])
 
     pos = nx.circular_layout(G)
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
-    nx.draw(G, pos, node_color='white', edge_color='black', node_size=1000, with_labels=True)
+    # nx.draw_networkx_edges(G, pos, edge_color=edgecolor, connectionstyle='arc3,rad=0.3')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, label_pos=0.22)
+
+    nx.draw(G, pos, node_color='white', edge_color=edgecolor, node_size=500, with_labels=True)
     ax = plt.gca()
     ax.collections[0].set_edgecolor("#000000")
 
     return G
 
 
-def adjacency_matrix(dictionary, nb_var):
+def weighted_adjacency_matrix(dictionary, nb_var):
     adj_matrix = np.zeros((nb_var, nb_var))
     for pair in dictionary:
         if dictionary.get(pair) == 0:
-            adj_matrix[pair[1], pair[0]] = -1
+            adj_matrix[pair[1], pair[0]] = 0.5
         else:
             adj_matrix[pair[1], pair[0]] = dictionary.get(pair)
     return adj_matrix
@@ -187,26 +205,252 @@ def SaveResults():
     return
 
 
+def compare_graphs(graph1, graph1name, graph2, graph2name, nb_var, cmp_to_gt=False, selfcauses=True):
+    readg2, delaysgraph1 = getdelays(graph1, nb_var)
+    readg1, delaysgraph2 = getdelays(graph2, nb_var)
+
+    if cmp_to_gt:
+        # When comparing a graph with its gt, g1 is the gt and g2 is the predicted graph
+        delaysgraph2 = manage_unoriented_edges(readg1, delaysgraph1, readg2, delaysgraph2)
+
+    commonedges = dict()
+    for i in delaysgraph1:
+        if i in delaysgraph2:
+            if delaysgraph1[i] == delaysgraph2[i]:
+                commonedges[i] = delaysgraph1[i]
+
+            else:
+                commonedges[i] = f"{delaysgraph1[i]},{delaysgraph2[i]}"
+
+    for i in commonedges:
+        del delaysgraph1[i]
+        del delaysgraph2[i]
+
+    plotgraph(commonedges, nb_var, 'black', selfcauses)
+    plotgraph(delaysgraph1, nb_var, 'red', selfcauses)
+    plotgraph(delaysgraph2, nb_var, 'blue', selfcauses)
+
+    commonpatch = mpatches.Patch(color='black', label='Common edges')
+    g1patch = mpatches.Patch(color='red', label=graph1name)
+    g2patch = mpatches.Patch(color='blue', label=graph2name)
+    plt.legend(handles=[commonpatch, g1patch, g2patch])
+
+
+def manage_unoriented_edges(readgt, gtdelays, readpred, preddelays):
+    supress_edges = []
+    for i in preddelays:
+        if (i[1], i[0]) in preddelays and i[0] != i[1]:
+            if (i in gtdelays) and ((i[1], i[0]) not in gtdelays):
+                supress_edges.append(i)
+            elif ((i[1], i[0]) in gtdelays) and (i not in gtdelays):
+                supress_edges.append((i[1], i[0]))
+
+    supress_edges = list(set(supress_edges))
+    for i in supress_edges:
+        del preddelays[i]
+        readpred[i[0]].remove(i[1])
+
+    return preddelays
+
+
 if __name__ == "__main__":
+    #TCDF Evaluation
 
-    architecture = "Fork"
-    nb_of_variables = 3
-
-    for file in os.listdir(f"Results/PCMCI+_Results_{architecture}"):
-        if file.endswith("groundtruth.csv"):
-            gt_file = file
-
-    dir = os.listdir(f"Results/PCMCI+_Results_{architecture}/gpdc")
-
+    folder="tunningts=1000"
+    architecture_list = ["Fork", "Mediator", "Vstructure", "Diamond", "7TS", "7ts2h"]
+    nb_of_variables_list = [3, 3, 3, 4, 7, 7]
+    dir = os.listdir(
+        f"C:/Users/flori/Downloads/causal_discovery_for_time_series-master/causal_discovery_for_time_series-master/baselines/scripts_python/python_packages/TCDF-master/TCDF-Master/{folder}")
+    tocsv = [
+        ["filename", "TPR", "FPR", "Precision on the edges", "Recall", "F1 score",
+         "Precision on the predicted lags",
+         "Frobenius Norm", "MSE", "SID"]]
     for file in dir:
         if file.endswith('.csv'):
+            for arch in architecture_list:
+                if remove_suffix(file,f"{remove_suffix(file,'.csv')[-1]}.csv")==arch:
+                    architecture=arch
+            nb_of_variables = nb_of_variables_list[architecture_list.index(architecture)]
+            for gtfile in os.listdir(f"Results/groundtruth"):
+                if gtfile.endswith(f"{architecture}_groundtruth.csv"):
+                    gt_file = gtfile
+
             print('Evaluation of file : ', file)
-            tau_max = int(file.rpartition('=')[2].rpartition('_')[0])
-            print('tau_max = ', tau_max)
-            gtpddata = pd.read_csv(f"Results/PCMCI+_Results_{architecture}/{gt_file}", header=None)
-            predfile = pd.read_csv(f"Results/PCMCI+_Results_{architecture}/gpdc/{file}", header=None)
-            evaluate(gtpddata,
-                            predfile,
-                            nb_var=nb_of_variables, tau_max=tau_max)
+            tau_max = 16
+            # print('tau_max = ', tau_max)
+            gtpddata = pd.read_csv(f"Results/groundtruth/{gt_file}", header=None)
+            try:
+                predfile = pd.read_csv(
+                    f"C:/Users/flori/Downloads/causal_discovery_for_time_series-master/causal_discovery_for_time_series-master/baselines/scripts_python/python_packages/TCDF-master/TCDF-Master/{folder}/{file}",
+                    header=None)
+                save = evaluate(gtpddata,
+                                predfile,
+                                nb_var=nb_of_variables, tau_max=tau_max)
+                save = [round(x, 2) for x in save]
+                save.insert(0, remove_suffix(file, '.csv'))
+                tocsv.append(save)
+
+                compare_graphs(gtpddata, "Ground truth graph", predfile,
+                               remove_suffix(file.rpartition('_')[2], '.csv'),
+                               nb_of_variables, cmp_to_gt=True)
+
+                #plt.savefig(
+                #    f"Results/tau_max=1\PCMCI+_1000points/PCMCI+_Results_{architecture}/gpdc/{remove_suffix(file.rpartition('_')[2], '.csv')}.png")
+
+                #plt.close()
 
 
+            except:
+                print("No predictions or decoding error for file", file)
+
+    saveframe = pd.DataFrame(tocsv)
+    saveframe = saveframe.transpose()
+
+    saveframe.to_csv(f"C:/Users/flori/Downloads/causal_discovery_for_time_series-master/causal_discovery_for_time_series-master/baselines/scripts_python/python_packages/TCDF-master/TCDF-Master/{folder}/Evaluation.csv",
+                         index=False,
+                         header=False)
+
+
+    '''
+    #PC evaluation
+    architecture_list = ["Fork", "Mediator", "Vstructure", "Diamond", "7TS", "7TS2H"]
+    nb_of_variables_list = [3, 3, 3, 4, 7, 7]
+
+    for i in range(len(architecture_list)):
+        architecture = architecture_list[i]
+        nb_of_variables = nb_of_variables_list[i]
+        for file in os.listdir(f"Results/groundtruth"):
+            if file.endswith(f"{architecture}_groundtruth.csv"):
+                gt_file = file
+
+        dir = os.listdir(f"Results/tau_max=1/PCMCI+_1000points/PCMCI+_Results_{architecture}/gpdc")
+
+        tocsv = [
+            ["filename", "TPR", "FPR", "Precision on the edges", "Recall", "F1 score",
+             "Precision on the predicted lags",
+             "Frobenius Norm", "MSE", "SID"]]
+
+        for file in dir:
+            if file.endswith('.csv'):
+                print('Evaluation of file : ', file)
+                tau_max = int(file.rpartition('=')[2].rpartition('_')[0])
+                # print('tau_max = ', tau_max)
+                gtpddata = pd.read_csv(f"Results/groundtruth/{gt_file}", header=None)
+                try:
+                    predfile = pd.read_csv(f"Results/tau_max=1/PCMCI+_1000points/PCMCI+_Results_{architecture}\gpdc/{file}",
+                                           header=None)
+                    save = evaluate(gtpddata,
+                                    predfile,
+                                    nb_var=nb_of_variables, tau_max=tau_max)
+                    save = [round(x, 2) for x in save]
+                    save.insert(0, remove_suffix(file.rpartition('_')[2], '.csv'))
+                    tocsv.append(save)
+
+                    compare_graphs(gtpddata, "Ground truth graph", predfile,
+                                   remove_suffix(file.rpartition('_')[2], '.csv'),
+                                   nb_of_variables, cmp_to_gt=True)
+
+                    plt.savefig(
+                        f"Results/tau_max=1\PCMCI+_1000points/PCMCI+_Results_{architecture}/gpdc/{remove_suffix(file.rpartition('_')[2], '.csv')}.png")
+
+                    plt.close()
+
+
+                except:
+                    print("No predictions or decoding error for file", file)
+
+        saveframe = pd.DataFrame(tocsv)
+        saveframe = saveframe.transpose()
+
+        saveframe.to_csv(f"Results/tau_max=1\PCMCI+_1000points/PCMCI+_Results_{architecture}/Evaluation.csv", index=False,
+                         header=False)
+
+
+    #gtpddata = pd.read_csv(f"Results/groundtruth/7TS2H_groundtruth.csv", header=None)
+    predfile = pd.read_csv("C:/Users/flori/Desktop/Stage_2A/Code/Results/PCMCI+_Results_cereal_data/gpdc/tau_max=10_cereal_database_linear.csv",
+                           header=None)
+    nb_var = 112
+
+    #readgt, delays = getdelays(gtpddata, nb_var)
+    readpred, preddelays = getdelays(predfile, nb_var)
+
+    plotgraph(preddelays, nb_var, 'black', selfcauses=True)
+    # plt.show()
+    # plt.figure(figsize=(10,20))
+    #compare_graphs(gtpddata, "Ground truth graph", predfile, "7TS2H 3", nb_var,cmp_to_gt=True, selfcauses=False)
+    plt.show()
+
+    '''
+
+    '''
+    # Experiment with TCDF Filter
+    datapd = pd.read_csv("diff_cereal_database_linear.csv")
+    predfile = pd.read_csv(
+        "C:/Users/flori/Desktop/Stage_2A/Code/Results/LPCMCI_Results_cereal_data/gpdc/TCDF_linear_diff.csv",
+        header=None)
+    nb_var = 112
+    var_names = list(pd.read_csv("diff_cereal_database_linear.csv").columns)
+
+    filter=[]
+    for i in predfile.index:
+        line = predfile.iloc[i]
+        cause = var_names[line[0]]
+        consequence = var_names[line[1]]
+        lag = line[2]
+        print(f"{cause} causes {consequence} with a time lag of {lag}")
+        filter.append(cause)
+        filter.append(consequence)
+        #plt.plot(datapd.iloc[:, line[0]])
+        #plt.title(f"Cause {cause}")
+        #plt.figure()
+        #plt.plot(datapd.iloc[:, line[1]])
+        #plt.title(f"Consequence {consequence}")
+        #plt.show()
+    filter = list(set(filter))
+    print(filter)
+    print(len(filter))
+
+    datatcdf = pd.read_csv(
+        "C:/Users/flori/Desktop/Stage_2A/Code/Results/LPCMCI_Results_cereal_data/gpdc/TCDF_linear_diff.csv",
+        header=None)
+    for index, row in datatcdf.iterrows():
+        print(datapd.columns[row[0]],"causes",datapd.columns[row[1]],"with a time lag of",row[2] )
+    a=0
+
+    datapd= datapd[filter]
+    print(datapd.columns)
+
+
+
+    data=pd.read_csv(
+        "C:/Users/flori/Desktop/Stage_2A/Code/Results/LPCMCI_Results_cereal_data/gpdc/tau_max=10_LPCMCI_TCDFfilter.csv",
+        header=None)
+    print(data.index)
+    for index, row in data.iterrows():
+        print(datapd.columns[row[0]],"causes",datapd.columns[row[1]],"with a time lag of",row[2] )
+    a=0
+    '''
+
+
+
+    '''
+    filter_countries = ["poland", "spain", "finland", "sweden", "lithuania", "latvia", "denmark", "estonia", "austria",
+                        "croatia", "slovakia", "bulgaria", "belgium", "luxembourg", "netherlands", "cyprus"]
+    list_countries=[]
+    list_var=[]
+    for i in datapd.columns:
+        country = i.split(" ")[1]
+        if country not in filter_countries:
+            datapd = datapd.drop(i, axis=1)
+        else:
+            list_countries.append(country)
+            list_var.append(i)
+    print(set(list_countries))
+    print(list_var)
+    '''
+
+    #datapd.to_csv("cereal_database_TCDFfilter.csv", index=False)
+
+    # readpred, preddelays = getdelays(predfile, nb_var)
+    # plotgraph(preddelays, nb_var, 'black', selfcauses=True)
+    # plt.show()
